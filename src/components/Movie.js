282ac90c1bbb5b33192675e32334183e19bb8260
mac1228 <68986081+alissa1228@users.noreply.github.com>
2021-05-31 22:50:02 +0900

    add Movie component

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,8 +1,24 @@
 import React from "react";
+import styled from 'styled-components'
 import { Link } from "react-router-dom";
 
-export default ({ id }) => (
-  <div>
-    <Link to={`/${id}`}>{id}</Link>
+const Movie =  ({ data }) => (
+  <>
+  {data.map(item=>
+  <div key={item.id}>
+    <Link to={item.id}>
+      {item.id}
+    </Link>
   </div>
+
+  )}
+  </>
+  // <div>
+  //   <Link to={`/${id}`}>{id}</Link>
+  // </div>
 );
+
+export default Movie;
+
+
+
--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,5 +1,7 @@
 import React from "react";
-import { gql, useQuery } from "@apollo/client";
+import { gql, useQuery } from "@apollo/client"
+import styled from 'styled-components'
+import Movie from '../components/Movie'
 
 const GET_MOVIES = gql`
   {
@@ -10,13 +12,69 @@ const GET_MOVIES = gql`
   }
 `;
 
-export default () => {
+const Home = () => {
   const { loading, data } = useQuery(GET_MOVIES);
   console.log('data', data)
-  if (loading) {
-    return "loading...";
-  }
-  if (data && data.movies) {
-    return data.movies.map(item => <h1 key={item.id}>{item.id}</h1>);
-  }
+
+  return (
+    <Container>
+      <Header>
+        <h1>Apollo_Movie_App</h1>
+        <h2>Practicing GraphQL with Apollo!</h2>
+      </Header>
+      {loading && <Loading>Loading...</Loading>}
+      {/* {!loading && data.movies && data.movies.map(item=> 
+      <Movie key={item.id} id={item.id}>{item.id}</Movie>)} */}
+      {!loading && data.movies && 
+      <Movie data={data.movies}/>}
+    </Container>
+  )
+
 };
+
+export default Home;
+
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  align-items: center;
+`
+
+const Header = styled.header`
+  width: 100%;
+  text-align: center;
+  background: linear-gradient(to right, #bc4e9c, #f80759);
+  color : #fff;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+
+  > h1 {
+    font-size: 60px;
+    font-weight: 600;
+    margin-bottom : 10px;
+  }
+
+  > h2 {
+    font-size: 35px;
+  }
+
+  // &:hover {
+  // color : linear-gradient(#FFFFFF, #FFEFBA); 
+  // -webkit-background-clip: text;
+  // -webkit-text-fill-color: transparent;
+  // }
+
+
+`
+
+const Loading = styled.div`
+font-size: 18px;
+opacity: 0.5;
+font-weight: 500;
+margin-top: 10px;
+`
+
