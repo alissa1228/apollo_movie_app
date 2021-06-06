6e29bfb0e789e3c45cea8415d004964ef15ad6ab
mac1228 <68986081+alissa1228@users.noreply.github.com>
2021-06-07 03:08:53 +0900

    #3 Detail.js suggestion 추가 중

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -28,50 +28,77 @@ const Detail = () => {
 
     return(
       <Container>
-      <Column>
-        <Title>{loading? 'Loading....' : data.movie.title}</Title>
-        {!loading && 
+        <MovieCon>
+          <Column>
+          <Title>{loading? 'Loading....' : data.movie.title}</Title>
+          {!loading && 
         <>
         <Subtitle>{data?.movie?.language} · {data?.movie?.rating}</Subtitle>
         <Description>{data?.movie?.description_intro} </Description>
         </>
         }
-      </Column>
-      <Poster bg={data?.movie?.medium_cover_image}></Poster>
-      {data?.suggestions.map(s=><h1 key={s.id}>{s.id}</h1>)}
-    </Container>
+          </Column>
+          <Poster bg={data?.movie?.medium_cover_image}/>
+        </MovieCon>
+        <Recommend>
+          {data?.suggestions.map(item=>
+          <Suggestions key={item.id} subbg={item.medium_cover_image}>
+          </Suggestions>)
+
+          }
+        </Recommend>
+      </Container>
+
+    //   <Container>
+    //   <Column>
+    //     <Title>{loading? 'Loading....' : data.movie.title}</Title>
+    //     {!loading && 
+    //     <>
+    //     <Subtitle>{data?.movie?.language} · {data?.movie?.rating}</Subtitle>
+    //     <Description>{data?.movie?.description_intro} </Description>
+    //     </>
+    //     }
+    //   </Column>
+    //   <Poster bg={data?.movie?.medium_cover_image}></Poster>
+    //   {/* {data?.suggestions.map(s=><h1 key={s.id}>{s.id}</h1>)} */}
+    // </Container>
     )
 }
 
 export default Detail;
+
+
     
 const Container = styled.div`
-  height: 100vh;
   background-image: linear-gradient(-45deg, #d754ab, #fd723a);
   width: 100%;
+  height: 100vh;
   display: flex;
-  justify-content: space-around;
-  align-items: center;
-  color: white;
+  flex-direction: column;
+  justify-content: space-around; //moiveCon, Recommend의 간격을 조정.
+  align-items : center;
 `;
 
+const MovieCon = styled.div`
+  display: flex;
+  height: 100%;
+  justify-content: space-around;
+`
+
 const Column = styled.div`
-  margin-left: 10px;
-  width:60%;
+  width: 60%;
 `;
 
 const Title = styled.h1`
-  font-size: 50px;
-  margin-bottom: 15px;
+  
 `;
 
 const Subtitle = styled.h4`
-  font-size: 30px;
-  margin-bottom: 10px;
+  
 `;
 
 const Description = styled.p`
-  font-size: 20px;
+  
 `;
 
 const Poster = styled.div`
@@ -84,3 +111,61 @@ const Poster = styled.div`
   background-size: cover;
   background-position: center center
 `;
+
+const Recommend = styled.div`
+  width: 80%;
+  height: 300px;
+  display: grid;
+  grid-template-columns: repeat(4, 1fr);
+  grid-gap: 25px;
+  position: relative;
+  top: -50px;
+
+`
+
+const Suggestions = styled.div`
+  height: 300px;
+  background-image : url(${({subbg})=> subbg});
+  box-shadow: ${props => props.subbg && '0 19px 38px rgba(0,0,0,0.30), 0 15px 12px rgba(0,0,0,0.22)'};
+  border-radius: 10px;
+`
+
+// const Container = styled.div`
+//   height: 100vh;
+//   background-image: linear-gradient(-45deg, #d754ab, #fd723a);
+//   width: 100%;
+//   display: flex;
+//   justify-content: space-around;
+//   align-items: center;
+//   color: white;
+// `;
+
+// const Column = styled.div`
+//   margin-left: 10px;
+//   width:60%;
+// `;
+
+// const Title = styled.h1`
+//   font-size: 50px;
+//   margin-bottom: 15px;
+// `;
+
+// const Subtitle = styled.h4`
+//   font-size: 30px;
+//   margin-bottom: 10px;
+// `;
+
+// const Description = styled.p`
+//   font-size: 20px;
+// `;
+
+// const Poster = styled.div`
+//   width: 25%;
+//   height: 60%;
+//   border-radius: 10px;
+//   box-shadow: ${props => props.bg && '0 19px 38px rgba(0,0,0,0.30), 0 15px 12px rgba(0,0,0,0.22)'};
+//   background-color: transparent;
+//   background-image : url(${({bg})=> bg});
+//   background-size: cover;
+//   background-position: center center
+// `;
