67eedf6df93f37077f9e9716f630f55681e3bd0a
mac1228 <68986081+alissa1228@users.noreply.github.com>
2021-06-06 18:55:02 +0900

    #2 first branch commit

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -3,7 +3,7 @@ import { gql, useQuery } from "@apollo/client"
 import styled from 'styled-components'
 import Movies from '../components/Movies'
 
-//query에 variable이 하나도 없을 때 
+//query에 variable이 하나도 없을 때 && 영화 전체를 가져올 때
 const GET_MOVIES = gql`
   {
     movies {
